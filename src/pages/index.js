a45307518973b4b22ba27706a2f16e8b49a0ed88
Seop <seungseop@users.noreply.github.com>
2018-09-07 17:13:53 +0900

    react router로 SPA 실습

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+/**
+ * 다음 코드 컴포넌트를 불러온 후
+ * 동일한 이름으로 내보냅니다.
+ */
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Post } from './Post';
+export { default as Posts } from './Posts';
